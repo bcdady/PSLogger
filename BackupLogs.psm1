45ff9124376c3addd91179ef2e604be48b9baecf
bcdady <bryan+git@dady.us>
2015-11-01 22:55:18 -0700

    Fork PSLogger Module from former PowerShell-Modules repo
    PSLogger Module is it's own project

---
+++ BackupLogs.psm1
@@ -0,0 +1,122 @@
+﻿#Requires -Version 3.0 -Modules PSLogger
+
+New-Variable -Name LastLogBackup -Description 'TimeStamp of the last time the Backup-Logs function was processed' -Scope Global -Force;
+New-Variable -Name BackupCadence -Value 10 -Description 'Default recurrence frequency for running Backup-Logs function' -Scope Global -Force ;
+
+[bool]$backupNow = $true;
+function Backup-Logs {
+<#
+.SYNOPSIS
+    Extension of Sperry module, to simplify cleanup of log files (commonly referred to as log rotation in UNIX / Linux context)
+.DESCRIPTION
+    As part of the PSLogger module, this script (function) is a complement to the Write-Log function, and simplifies WindowsPowerShell log maintenance
+    By default, Backup-Logs will look for any/all files in $env:USERPROFILE\Documents\WindowsPowerShell\log, and if they're older than 7 days, move them into a archive\ subfolder
+    If necesarry (and if sufficient permissions are available), the archive\ subfolder will be created automatically
+    If the archive\ subfolder already exists, Backup-Logs will search for any files older than 90 days, and delete them.
+    Note: For both the log age and the archive purge age, each file's LastWriteTime property is what is evaluated
+    All of these conditions are customizable through parameters.
+    Invoke get-help Backup-Logs -examples for additional information
+.PARAMETER Path
+    Optionally specifies the 'root' path of where to look for and maintain log files, to be moved to \archive\.
+.PARAMETER Age
+    Optionally specifies age of log files to be moved to \archive\.
+.PARAMETER Purge
+    Optionally specifies a date, by age from today(), for which all older log files will be deleted.
+.EXAMPLE
+    PS .\> Backup-Logs 
+
+    Moves all .log files older than 7 days, from $env:USERPROFILE\Documents\WindowsPowerShell\log\ to $env:USERPROFILE\Documents\WindowsPowerShell\log\archive\
+.EXAMPLE
+    PS .\> Backup-Logs -age 14 -purge 90
+
+    Moves all .log files older than 14 days, to $env:USERPROFILE\Documents\WindowsPowerShell\log\archive\, and deletes all files from the archive folder which are older than 90 days
+.NOTES
+    NAME        :  BackupLogs.ps1
+    VERSION     :  1.0
+    LAST UPDATED:  4/9/2015
+    AUTHOR      :  Bryan Dady
+#>
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory=$false, Position=0)]
+        [string]
+        $path="$env:USERPROFILE\Documents\WindowsPowerShell\log",
+
+        [Parameter(Mandatory=$false, Position=1)]
+        [ValidateRange(0,9999)]
+        [int16]
+        $age=7,
+
+        [Parameter(Mandatory=$false, Position=2)]
+        [ValidateRange(0,9999)]
+        [int16]
+        $purge = 90,
+
+        [Parameter(Mandatory=$false, Position=3)]
+        [switch]
+        $force
+    )
+
+    Show-Progress -msgAction Start -msgSource $MyInvocation.MyCommand.Name
+
+    Write-Log -Message "Checking `$path: $path" -Function $MyInvocation.MyCommand.Name;
+    $path="$env:USERPROFILE\Documents\WindowsPowerShell\log"; # Degug only
+    if (Test-Path -Path "$path") {
+        # confirmed $path exists; see if \archive subfolder exists
+        if (Test-Path -Path "$path\archive") {
+            Write-Log -Message 'confirmed archive folder exists' -Function $MyInvocation.MyCommand.Name;
+            # set variable LastLogBackup based on the latest log file in $path\archive
+            $LastLogFile = Get-ChildItem -Path $path\archive -Filter *.log -File | Sort-Object -Property LastWriteTime -Descending | Select-Object -First 1;
+            Set-Variable -Name LastLogBackup -Value (Get-Date -Date $LastLogFile.LastAccessTime -DisplayHint Date -Format d) -PassThru; #s; #r; #y; #u;
+
+            Write-Log -Message "LastLogBackup was $LastLogBackup" -Function $MyInvocation.MyCommand.Name;
+            Set-Variable -Name NextBackupDate -Value (Get-Date -Date $LastLogBackup).AddDays($BackupCadence) -PassThru
+            Write-Log -Message "NextBackupDate is $NextBackupDate" -Function $MyInvocation.MyCommand.Name;
+
+            # Is today on or after $NextBackupDate ?
+            if ($NextBackupDate -ge (Get-Date)) {
+                # we DON'T need to backup right now
+                $backupNow = $false;
+            }
+        } else {
+            # log archive path doesn't yet exist, so create it
+            Write-Log -Message 'Creating archive folder' -Function $MyInvocation.MyCommand.Name;
+            New-Item -itemtype Directory -path $path\archive;
+            Set-Variable -Name LastLogBackup -Value (Get-Date -DisplayHint Date -Format d); #s; #r; #y; #u;
+            # Since we've never backed up to this path before, leave $backupNow = $true
+        }
+
+        if ($backupNow -or $force) {
+            # we can now proceed with backing up logs
+            $logFileDateString = get-date -UFormat '%Y%m%d';
+            Write-Log -Message "Archiving files older than $age days." -Function $MyInvocation.MyCommand.Name -Verbose;
+            Write-Log -Message " # # # BEGIN ROBOCOPY # # # # #`n" -Function $MyInvocation.MyCommand.Name;
+        
+            Write-Log -Message "About to run robocopy, logging to ""$path\Backup-Logs_$logFileDateString.log""" -Function $MyInvocation.MyCommand.Name;
+
+            & robocopy.exe """$path"" ""$path\archive"" /MINAGE:$age /MOV /R:1 /W:1 /NS /NC /NP /NDL /TEE" | Out-File -FilePath "$path\Backup-Logs_$logFileDateString.log" -Append -NoClobber; # /LOG+:$path\Backup-Logs_$logFileDateString.log
+
+            Write-Log -Message " # # # END ROBOCOPY # # # # #`n" -Function $MyInvocation.MyCommand.Name;
+
+            # Now we attempt to cleanup (purge) any old files
+            [System.DateTime]$purgeDate = (Get-Date).AddDays(-$purge);
+            Write-Log -Message "Purge date is $purgeDate" -Function $MyInvocation.MyCommand.Name;
+        
+            # Enumerate files, and purge those that haven't been updated wince $purge.
+            Write-Log -Message "Deleting archive\ files older than $purgeDate" -Function $MyInvocation.MyCommand.Name -Verbose;
+
+            Get-ChildItem -Path $path\archive -File | Where-Object {$_.LastWriteTime -lt $purgeDate} | Remove-Item -ErrorAction SilentlyContinue -WhatIf;
+
+        } else {
+            Write-Log -Message 'No need to archive log files right now.' -Function $MyInvocation.MyCommand.Name;
+        }
+    } else {
+        Write-Log -Message "Unable to confirm existence of logs folder: $path" -Function $MyInvocation.MyCommand.Name -Verbose;
+    }
+    
+    Show-Progress -msgAction Stop -msgSource $MyInvocation.MyCommand.Name
+}
+
+New-Alias -Name Archive-Logs -Value Backup-Logs -Description 'PSLogger Module' -ErrorAction SilentlyContinue;
+
+Export-ModuleMember -function Backup-Logs -Alias Archive-Logs;
---
+++ PSLogger.psm1
@@ -0,0 +1,258 @@
+﻿#requires -version 2.0
+<#
+.SYNOPSIS
+  PSLogger is a PowerShell module to enhance and simplify interacting with log files on the file system.
+.DESCRIPTION
+  PSLogger provides functions to Write logs, find / enumerate recently written log files, and Read the latest lines of recent .log files.
+.NOTES
+  NAME        :  PSLogger
+  VERSION     :  2.1.1   
+  LAST UPDATED:  5/12/2015
+  AUTHOR      :  Bryan Dady
+  Original Write-Log Author: Jeffery Hicks
+    http://jdhitsolutions.com/blog
+    http://twitter.com/JeffHicks
+    Date: 3/3/2011
+.LINK
+  https://github.com/bcdady/
+.LINK
+  http://bryan.dady.us
+.LINK
+  http://twittter.com/bcdady
+#>
+
+# Setup necessary configs for PSLogger's Write-Log cmdlet
+[string]$loggingPreference='Continue'; # set $loggingPreference to anything other than continue, to leverage write-debug or write-verbose, without writing to a log on the filesystem
+[string]$loggingPath = "$env:userprofile\Documents\WindowsPowerShell\log"
+[string]$logFileDateString = get-date -UFormat '%Y%m%d'; # Need to keep an eye on this one, in case PowerShell sessions run for multiple days, I doubt this variable value will be refreshed / updated
+# [string]$LastFunction;
+New-Variable -Name LastFunction -Description "Retain 'state' of the last function name called, to streamline logging statements from the same function" -Force -Scope Global -Visibility Public
+[bool]$writeIntro=$true
+[string]$global:LastFunction;
+
+Function Write-Log {
+<#
+   .Synopsis
+        Write a message to a log file. 
+    .Description
+        Write-Log can be used to write text messages to a log file. It can be used like Write-Verbose,
+        and looks for two variables that you can define in your scripts and functions. If the function
+        finds $LoggingPreference with a value of "Continue", the message text will be written to the file.
+        The default file is PowerShellLog.txt in your %TEMP% directory. You can specify a different file
+        path by parameter or set the $logFilePref variable. See the help examples.
+    
+        This function also supports Write-Verbose which means if -Verbose is detected, the message text
+        will be written to the Verbose pipeline. Thus if you call Write-Log with -Verbose and a the 
+        $loggingPreference variable is set to continue, you will get verbose messages AND a log file.
+    .Parameter Message
+        The message string to write to the log file. It will be prepended with a date time stamp.
+    .Parameter Path
+        The filename and path for the log file. The default is $env:temp\PowerShellLog.txt, 
+        unless the $logFilePref variable is found. If so, then this value will be used.
+   .Example
+        PS C:\> . c:\scripts\write-log.ps1
+    
+    Here is a sample function that uses the Write-Log function after it has been dot sourced. Within the sample 
+    function, the logging variables are defined.
+    
+Function TryMe {
+    [cmdletbinding()]
+    Param([string]$computername=$env:computername,
+    [string]$Log
+    )
+    if ($log) 
+    {
+     $loggingPreference="Continue"
+     $logFilePref=$log
+    }
+    Write-log "Starting Command"
+    Write-log "Connecting to $computername"
+    $b=gwmi win32_bios -ComputerName $computername
+    $b
+    Write-log $b.version
+    Write-Log "finished" $log
+}
+
+TryMe -log e:\logs\sample.txt -verbose
+  
+   .Notes
+    NAME: Write-Log
+    AUTHOR: Jeffery Hicks
+    VERSION: 1.0
+    LASTEDIT: 03/02/2011
+    
+    Learn more with a copy of Windows PowerShell 2.0: TFM (SAPIEN Press 2010)
+    
+   .Link
+       http://jdhitsolutions.com/blog/2011/03/powershell-automatic-logging/
+    
+    .Link
+        Write-Verbose
+#>
+    [cmdletbinding()]
+    Param(
+        [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true, HelpMessage='The message string to write to the log file. It will be prepended with a date time stamp.')]
+        [ValidateNotNullOrEmpty()]
+        [string]$Message,
+
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=1,
+                   HelpMessage='The Function Parameter passes the name of the Function or CmdLet that invoked this Write-Log function')]
+        [ValidateNotNullOrEmpty()]
+        [Alias('Action', 'Source')]
+        [String[]]
+        $Function,
+
+        [Parameter(Position=2,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   HelpMessage='The optional Path parameter specifies the path of the log file to write the message to.')]
+        [string[]]
+	    $Path="$env:userprofile\Documents\WindowsPowerShell\log\PowerShell.log"
+
+    )
+
+    # Detect -debug mode:
+    # http://blogs.msdn.com/b/powershell/archive/2009/04/06/checking-for-bound-parameters.aspx
+    # https://kevsor1.wordpress.com/2011/11/03/powershell-v2-detecting-verbose-debug-and-other-bound-parameters/
+    if ($PSBoundParameters['Debug'].IsPresent) {
+	    [bool]$testMode = $true; 
+        $logFilePref = Join-Path -Path "$loggingPath\test" -ChildPath "$("$Function", "$logFileDateString" -join '_').log"
+    } else {
+	    [bool]$testMode = $false; 
+        $logFilePref = Join-Path -Path "$loggingPath" -ChildPath "$("$Function", "$logFileDateString" -join '_').log";
+    }
+
+    # Assign Function variable to 'PowerShell' if blank or null
+    if (($Function -eq $null) -or ($Function -eq '')) { $Function = 'PowerShell' }
+
+    # Detect if this Function is the same as the $LastFunction. If not, verbosely log which new Function is active
+    if ($Function -eq $LastFunction) {
+        # Retain 'state' of the last function name called, to streamline logging statements from the same function
+#        Write-Debug -Message "Function is $Function";
+#        Write-Debug -Message "LastFunction is $LastFunction";
+        $writeIntro=$false;
+    } else {
+#        Write-Debug -Message "Shifting from LastFunction $LastFunction to $Function, so `$writeIntro=`$true";
+        Set-Variable -Name LastFunction -Value $Function -Force -Scope Global
+        $writeIntro=$true;
+#        Write-Output -Message "LastFunction is now $LastFunction";
+    }
+
+    #Pass on the message to Write-Debug cmdlet if -Debug parameter was used
+    if ($testMode) {
+        Write-Debug -Message $Message;
+        if ($writeIntro -and ( $Message -notlike 'Exit*')) {Write-Host -Object "Logging [Debug] to $logFilePref`n" };
+    } elseif ($PSBoundParameters['Verbose'].IsPresent) {
+        #Pass on the message to Write-Verbose cmdlet if -Verbose parameter was used
+        Write-Verbose -Message $Message;
+        if ($writeIntro -and ( $Message -notlike 'Exit*')) { Write-Host -Object "Logging to $logFilePref`n" };
+    }
+
+    #only write to the log file if the $LoggingPreference variable is set to Continue
+    if ($LoggingPreference -eq 'Continue') {
+    
+        #if a $logFilePref variable is found in the scope hierarchy then use that value for the file, otherwise use the default $path
+        if ($logFilePref) {
+		    $LogFile=$logFilePref
+        } else {
+		    $LogFile=$Path
+        }
+        if ($testMode) {
+            Write-Output "$(Get-Date) [Debug] $Message" -NoEnumerate | Out-File -FilePath $LogFile -Append
+        } elseif ($PSBoundParameters['Verbose'].IsPresent) {
+            Write-Output "$(Get-Date) [Verbose] $Message" -NoEnumerate | Out-File -FilePath $LogFile -Append
+        } else {
+            Write-Output "$(Get-Date) $Message" -NoEnumerate | Out-File -FilePath $LogFile -Append
+        }
+    }
+
+} #end function
+
+Function Read-Log {
+<#
+   .Synopsis
+        Reads the latest log file, optionally displaying only the latest number of specified lines
+    .Description
+        Intended as a complement to the Write-Log function provided within the PSLogger module, Read-Log will find the latest log file in the
+        defined $loggingPath directory, and return some basic stats of that file, as well as get it's contents for review
+    .Parameter MessageSource
+        The message source is an optional parameter that specifies which module, function, or script wrote the log file to be retrieved.
+        If this parameter is not specified, function returns the latest available log file, regardless of message source.
+        The value of this parameter becomes a filter to the search of .log files within the $loggingPath directory.
+    .Parameter lineCount
+        The most recent number of lines from the log file in question.
+        unless the $logFilePref variable is found. If so, then this value will be used.
+   .Example
+        PS .\> Read-Log
+
+        Returns basic file properties, and last 10 lines, of the latest / newest log file found in $loggingPath directory
+
+   .Example
+        PS .\> Read-Log -MessageSource Get-Profile -lineCount 30
+
+        Returns latest log file, reading the latest 30 lines, specific to function Get-Profile
+       
+   .Notes
+        NAME: Read-Log
+        AUTHOR: Bryan Dady
+        VERSION: 1.0
+        LASTEDIT: 04/15/2015
+
+    .Output
+        Matches default properties return the same as Get-Item:
+        * Name
+        * LastWriteTime
+        * Length
+        * Path     
+#>
+    [cmdletbinding()]
+    Param(
+        [Parameter(
+            Mandatory=$false,
+            Position=0,
+            HelpMessage='The message source typically matches a log filename prefix, which represents the name of the function or module which wrote the log file.'
+        )]
+        [Alias('function','source','f','m')]
+        [string]
+        $MessageSource,
+
+        [Parameter(
+            Mandatory=$false, 
+            Position=1,
+            HelpMessage='Provide an integer of line numbers to read from the bottom of the log file.'
+        )]
+        [ValidateNotNullOrEmpty()]
+        [Alias('lines', 'l')]
+        [int16]
+        $lineCount=10
+    )
+    # Use write-output instead of write-log, so that the function of reading log files does not write new log files to be read
+    Write-Output -InputObject "Selecting latest log file to read last $lineCount lines";
+
+    # Select the newest (1) file (most recent LastWriteTime), with an optional filter, based on MessageSource parameter
+    $latestLogFile = Get-ChildItem -Path $loggingPath -Filter *$MessageSource* -File | Sort-Object -Property LastWriteTime -Descending | Select-Object -First 1;
+
+    if (Test-Path -Path $latestLogFile -PathType Leaf -IsValid) {
+        Write-Output -InputObject "Selected $latestLogFile";
+        $latestLogFile | Select-Object -Property Name,LastWriteTime | Format-List
+        if ($lineCount -gt 0) {
+            Write-Output -InputObject "`n ... ";
+            $latestLogFile | Get-Content -Tail $lineCount;
+            Write-Output -InputObject "`n[EOF]`n";
+        }
+    } else {
+        Write-Warning -Message "Could not open $latestLogFile for reading";
+    }
+
+} #end function
+
+Function Get-LatestLogs {
+    Get-ChildItem $env:userprofile\Documents\WindowsPowerShell\log | Sort-Object -Descending -Property LastWriteTime | Select-Object -First 10
+}
+
+Export-ModuleMember -function *
